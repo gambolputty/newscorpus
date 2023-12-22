cfa7acfb60df19d13c5e79c2bf847df8b2156750
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2023-12-22 19:21:24 +0100

    show logging output when not in debug mode

--- newscorpus/logger.py
+++ newscorpus/logger.py
@@ -11,7 +11,7 @@ def create_rotating_log(debug: bool = False):
     LOG_DIR_PATH.mkdir(exist_ok=True)
 
     logger = logging.getLogger("rotating_log")
-    logger.setLevel(logging.INFO if not debug else logging.DEBUG)
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
 
     rotation_handler = RotatingFileHandler(
         LOG_DIR_PATH.joinpath("scraper_log"),
@@ -24,14 +24,13 @@ def create_rotating_log(debug: bool = False):
     )
     logger.addHandler(rotation_handler)
 
-    # add stream handler in debug mode
-    if debug:
-        stream_handler = colorlog.StreamHandler()
-        stream_handler.setFormatter(
-            colorlog.ColoredFormatter(
-                "%(log_color)s%(levelname)s: %(message)s"
-            )  # noqa: E501
-        )
-        logger.addHandler(stream_handler)
+    # add stream handler for colored output
+    stream_handler = colorlog.StreamHandler()
+    stream_handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(log_color)s%(levelname)s: %(message)s"
+        )  # noqa: E501
+    )
+    logger.addHandler(stream_handler)
 
     return logger
