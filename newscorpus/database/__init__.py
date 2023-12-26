1cd079a322c0b26a7ef0d42253e226bac4af966a
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2023-12-27 00:37:26 +0100

    add tqdm

--- newscorpus/database/__init__.py
+++ newscorpus/database/__init__.py
@@ -5,6 +5,7 @@ from typing import cast
 from pydantic import BaseModel, field_serializer
 from sqlite_utils import Database as SQLiteUtilsDatabase
 from sqlite_utils.db import Table
+from tqdm import tqdm
 
 DEFAULT_DATABASE_PATH = (
     Path(__file__).parent.parent.parent.resolve().joinpath("newscorpus.db")
@@ -96,7 +97,7 @@ class Database:
 
         with open(json_file_path, "r") as f:
             try:
-                for line in f:
+                for line in tqdm(f):
                     old_article = json.loads(line)
                     published_at = parse(old_article["published_at"]["$date"])
 
@@ -133,3 +134,43 @@ class Database:
             finally:
                 db.create_indices()
                 db.close()
+
+    @staticmethod
+    def _import_with_url_check_articles_from_json(json_file_path: Path):
+        """
+        Import articles from old MongoDB json file
+        """
+        import json
+
+        from dateparser import parse
+
+        db = Database()
+        articles = []
+
+        with open(json_file_path, "r") as f:
+            for line in tqdm(f):
+                old_article = json.loads(line)
+                published_at = parse(old_article["published_at"]["$date"])
+
+                if not published_at:
+                    print("Could not parse date")
+                    continue
+
+                # skip articles older than n days
+                difference = datetime.datetime.now() - published_at
+                if difference.days > 14:
+                    continue
+
+                new_article = Article(
+                    title=old_article["title"],
+                    description=None,
+                    text=old_article["text"],
+                    url=old_article["url"],
+                    published_at=published_at,
+                    source=old_article["src"],
+                )
+                articles.append(new_article.model_dump())
+
+        db.insert_articles(articles)
+
+        db.close()
